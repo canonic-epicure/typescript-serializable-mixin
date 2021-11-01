be6961be499aaef622f7f79d894dab851b7871b6
nickolay <nickolay8@gmail.com>
2021-11-01 09:32:35 +0300

    - special compilation mode of the whole repo

---
+++ scripts/tweak_dts.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# exit if any of command has failed
+set -e
+# enable **, ! in globs
+shopt -s globstar extglob
+
+DIR="$( cd "$( dirname "$0" )" && pwd )"
+cd "$DIR/.."
+
+echo ">> Tweaking *.d.ts files"
+
+#━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+# Very very horrible mess caused by: https://github.com/microsoft/TypeScript/issues/35822
+
+#━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+# Isomorphic
+#━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+sed -i -e 's!"prototype" | "fromJSON"!"fromJSON"!' "src/serializable/Serializable.d.ts"
+
